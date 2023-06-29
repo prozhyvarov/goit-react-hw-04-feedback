084e5e65bb57c1573de097424e9f63e5b0af994a
prozhyvarov <121400043+prozhyvarov@users.noreply.github.com>
2023-06-29 05:07:19 -0600

    update

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { List, Item } from './Statistics.styled';
 
-export const Statistics = ({ good, neutral, bad, total, positivePercentage }) => {
+export const Statistics = ({ good, neutral, bad, total, }) => {
   return (
     <List>
       <Item>Good: {good}</Item>
       <Item>Neutral: {neutral}</Item>
       <Item>Bad: {bad}</Item>
       <Item>Total: {total}</Item>
-      <Item>Positive Feedback: {positivePercentage}%</Item>
+      <Item>Positive Feedback: {Math.round((good / total) * 100) || 0}%</Item>
     </List>
   );
 };
